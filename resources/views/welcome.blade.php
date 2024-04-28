2331ff36eee34eff3dfdea9878f14d26464a42d9
Naio200000 <90395089+Naio200000@users.noreply.github.com>
2024-04-27 23:30:20 -0300

    feat: add home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,116 +1,33 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Tienda Komei :: Home</title>
-        <link rel="stylesheet" href="css/bootstrap.min.css">
-        <link rel="stylesheet" href="css/styles.css">
-    </head>
-    <body>
-        <header class="header">
-            <nav class="navbar navbar-expand-md fixed-top container-fluid x-auto ">
-                <div class="nav container align-items-center">
-                    {{-- <a class="navbar-brand " href="index.php?view=home"><img src="./img/logo/komei_Juku.webp" alt="Logo de la escuela"></a> --}}
-                    <h1 class="titulo text-uppercase text-center">Komei Juku</h1>
-                    <button class="navbar-toggler ms-auto m-2" type="button" data-bs-toggle="collapse" data-bs-target="#ContenidoSoporteBarranNav" aria-controls="ContenidoSoporteBarranNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                    </button>
-
-                    <div class="order-md-last minicarrito d-none d-lg-flex justify-content-lg-evenly align-items-lg-center">
-                        <div class=" text-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-currency-dollar" viewBox="0 0 16 16">
-                                <path d="M4 10.781c.148 1.667 1.513 2.85 3.591 3.003V15h1.043v-1.216c2.27-.179 3.678-1.438 3.678-3.3 0-1.59-.947-2.51-2.956-3.028l-.722-.187V3.467c1.122.11 1.879.714 2.07 1.616h1.47c-.166-1.6-1.54-2.748-3.54-2.875V1H7.591v1.233c-1.939.23-3.27 1.472-3.27 3.156 0 1.454.966 2.483 2.661 2.917l.61.162v4.031c-1.149-.17-1.94-.8-2.131-1.718H4zm3.391-3.836c-1.043-.263-1.6-.825-1.6-1.616 0-.944.704-1.641 1.8-1.828v3.495l-.2-.05zm1.591 1.872c1.287.323 1.852.859 1.852 1.769 0 1.097-.826 1.828-2.2 1.939V8.73l.348.086z"/>
-                            </svg>
-                            <span class="minicarrito-precio">0</span>
-                        </div>
-                        <div class="text-center">
-                            <button type="button" class="btn btn-komei position-relative px-3">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
-                                <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
-                            </svg>
-                            <span class="minicarrito-cantidad position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">0</span>
-                            </button>
-                        </div>
-                    </div>
-                    <div class="collapse navbar-collapse ms-5" id="ContenidoSoporteBarranNav">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
-                                <a class="nav-link active text-center text-sm-end" aria-current="page" href="#">Home</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link active text-center text-sm-end" aria-current="page" href="#">Nosotros</a>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link active text-center text-sm-end dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Productos</a>
-                                <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="#">Catalogo Completo</a></li>
-                                    <li><a class="dropdown-item text-capitalize" href="#">Clases</a></li>
-                                    <li><a class="dropdown-item text-capitalize" href="#">Ropa</a></li>
-                                    <li><a class="dropdown-item text-capitalize" href="#">Equipos</a></li>
-                                </ul>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link active text-center text-sm-end " aria-current="page" href="index.php?view=dojos">Contactos</a>
-                            </li>
-                        </ul>
-                    </div>
+<x-template>
+    <x-slot:title>Home::</x-slot:title>
+    <section class="hero">
+        <div id="CarrpuselHero" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active" data-bs-interval="10000">
+                    <img src="./img/hero/komei-juku-hero-2.webp" class="d-block w-100" alt="Grupo de estdiantes de Iaido">
                 </div>
-            </nav>
-            <nav class="fixed-bottom navbar d-block d-lg-none">
-                <div class="minicarrito row align-items-center my-3">
-                    <div class="col">
-                        <div class="text-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-currency-dollar" viewBox="0 0 16 16">
-                                <path d="M4 10.781c.148 1.667 1.513 2.85 3.591 3.003V15h1.043v-1.216c2.27-.179 3.678-1.438 3.678-3.3 0-1.59-.947-2.51-2.956-3.028l-.722-.187V3.467c1.122.11 1.879.714 2.07 1.616h1.47c-.166-1.6-1.54-2.748-3.54-2.875V1H7.591v1.233c-1.939.23-3.27 1.472-3.27 3.156 0 1.454.966 2.483 2.661 2.917l.61.162v4.031c-1.149-.17-1.94-.8-2.131-1.718H4zm3.391-3.836c-1.043-.263-1.6-.825-1.6-1.616 0-.944.704-1.641 1.8-1.828v3.495l-.2-.05zm1.591 1.872c1.287.323 1.852.859 1.852 1.769 0 1.097-.826 1.828-2.2 1.939V8.73l.348.086z"/>
-                            </svg>
-                            <span class="minicarrito-precio">0</span>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="text-center">
-                            <button type="button" class="btn btn-komei position-relative px-3">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
-                                <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
-                            </svg>
-                                <span class="minicarrito-cantidad position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">0</span>
-                            </button>
-                        </div>
-                    </div>
+                <div class="carousel-item" data-bs-interval="2000">
+                    <img src="./img/hero/komei-juku-hero-3.webp" class="d-block w-100" alt="Banner de promocion para clases">
                 </div>
-            </nav>
-        </header>
-        <main class="hero">
-            <h2>Bienvenidos</h2>
-        </main>
-        <footer class="container-fluid mb-lg-0 footer">
-            <div class="container-md">
-                <div class="row align-items-center">
-                    <div class="col col-lg-4 p-1">
-                    <img class="imagen-perfil footer-perfil" src="./img/misc/perfil.webp" alt="Un señor muy buen moso">
-                    </div>
-                    <div class="col">
-                        <div class="row">
-                            <div class="col-12 col-lg-5">
-                            <ul class="datos">
-                                <li>Nicolas,Alsinet</li>
-                                <li>38 Años</li>
-                                <li>nicolas.alsinet@davinci.edu.ar</li>
-                            </ul>
-                            </div>
-                            <div class="col-12 col-lg-7">
-                                <ul class="row social">
-                                    <li class="col"><a href="https://www.instagram.com/nicolas.alsinet/" target="_blank"><img src="./img/logo/SVG/Instagram_logo_2016.svg" alt="Logo de Instagram"></a></li>
-                                    <li class="col"><a href="https://www.linkedin.com/in/nicolas-alsinet-99a067226/" target="_blank"><img src="./img/logo/SVG/LinkedIn_icon.svg" alt="Logo de linkedin"></a></li>
-                                    <li class="col"><a href="https://github.com/Naio200000" target="_blank"><img src="./img/logo/SVG/github_icon.svg" alt="Logo de Github"></a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
+                <div class="carousel-item">
+                    <img src="./img/hero/komei-juku-hero-4.webp" class="d-block w-100" alt="Primer plano de persona a punto de desenvainar una katana">
                 </div>
             </div>
-        </footer>
-        <script src="js/bootstrap.bundle.js"></script>
-    </body>
-</html>
+            <button class="carousel-control-prev" type="button" data-bs-target="#CarrpuselHero" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Anterior</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#CarrpuselHero" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Proxima</span>
+            </button>
+        </div>
+    </section>
+    <section class="home container-fluid container-md">
+        <h2 class="titulo-seccion w-75 w-lg-100 text-uppercase text-center mt-2 mb-5 mx-auto px-2">Escuela de Iaido - Esgrima Samurai</h2>
+        <p class="pe-md-3 fs-5 w-75 mx-auto"><strong>Komei Juku Argentina</strong> es una escuela esgrima japonesa especializada en el estilo <em>Muso Jikiden Eishin Ryu</em>.</p>
+        <p class="pe-md-3 fs-5 w-75 mx-auto">Ofresemos <strong>clases</strong> en varios dojos, <strong>ropa</strong> de practica y <strong>equipos</strong> en general.</p>
+        <p class="pe-md-3 fs-5 w-75 mx-auto"><a href="index.php?view=dojos" class=""><em>Contactanos</em></a> por cualquier duda que tengas, estamos a tu disposición.</p>
+    </section>
+
+</x-template>
