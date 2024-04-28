6e653e6d1239eddda062d9e99494fcfc976fca2e
Naio200000 <90395089+Naio200000@users.noreply.github.com>
2024-04-27 23:10:47 -0300

    feat: add footer template

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,7 @@
         <header class="header">
             <nav class="navbar navbar-expand-md fixed-top container-fluid x-auto ">
                 <div class="nav container align-items-center">
-                    <a class="navbar-brand " href="index.php?view=home"><img src="./img/logo/komei_Juku.webp" alt="Logo de la escuela"></a>
+                    {{-- <a class="navbar-brand " href="index.php?view=home"><img src="./img/logo/komei_Juku.webp" alt="Logo de la escuela"></a> --}}
                     <h1 class="titulo text-uppercase text-center">Komei Juku</h1>
                     <button class="navbar-toggler ms-auto m-2" type="button" data-bs-toggle="collapse" data-bs-target="#ContenidoSoporteBarranNav" aria-controls="ContenidoSoporteBarranNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
@@ -81,6 +81,36 @@
                 </div>
             </nav>
         </header>
+        <main class="hero">
+            <h2>Bienvenidos</h2>
+        </main>
+        <footer class="container-fluid mb-lg-0 footer">
+            <div class="container-md">
+                <div class="row align-items-center">
+                    <div class="col col-lg-4 p-1">
+                    {{-- <img class="imagen-perfil footer-perfil" src="./img/misc/perfil.webp" alt="Un señor muy buen moso"> --}}
+                    </div>
+                    <div class="col">
+                        <div class="row">
+                            <div class="col-12 col-lg-5">
+                            <ul class="datos">
+                                <li>Nicolas,Alsinet</li>
+                                <li>38 Años</li>
+                                <li>nicolas.alsinet@davinci.edu.ar</li>
+                            </ul>
+                            </div>
+                            {{-- <div class="col-12 col-lg-7">
+                                <ul class="row social">
+                                    <li class="col"><a href="https://www.instagram.com/nicolas.alsinet/" target="_blank"><img src="./img/logo/SVG/Instagram_logo_2016.svg" alt="Logo de Instagram"></a></li>
+                                    <li class="col"><a href="https://www.linkedin.com/in/nicolas-alsinet-99a067226/" target="_blank"><img src="./img/logo/SVG/LinkedIn_icon.svg" alt="Logo de linkedin"></a></li>
+                                    <li class="col"><a href="https://github.com/Naio200000" target="_blank"><img src="./img/logo/SVG/github_icon.svg" alt="Logo de Github"></a></li>
+                                </ul>
+                            </div> --}}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
         <script src="js/bootstrap.bundle.js"></script>
     </body>
 </html>
