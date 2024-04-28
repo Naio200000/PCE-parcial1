00dc9541abc2f615be68538e987e50e293d7b043
Naio200000 <90395089+Naio200000@users.noreply.github.com>
2024-04-27 21:22:01 -0300

    fixed bugs

--- app/Http/Controllers/TiendaController.php
+++ app/Http/Controllers/TiendaController.php
@@ -8,6 +8,6 @@ class TiendaController extends Controller{
 
     public function index () {
 
-        return view('tienda.index');
+        return view('store.index');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [\App\Http\Controllers\HomeController::class,'home']);
-Route::get('nostros', [\App\Http\Controllers\HomeController::class,'about']);
+Route::get('nosotros', [\App\Http\Controllers\HomeController::class,'about']);
 Route::get('contactos', [\App\Http\Controllers\HomeController::class,'contacts']);
 
-Route::get('tienda', [\App\Http\Controllers\TiendaController::class, 'idenx']);
+Route::get('tienda', [\App\Http\Controllers\TiendaController::class, 'index']);
