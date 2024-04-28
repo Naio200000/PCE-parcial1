cf24f1d3a1083c5f569f0c7c8598e8b7ed75aa19
Naio200000 <90395089+Naio200000@users.noreply.github.com>
2024-04-28 14:36:02 -0300

    feat: add tienda page

--- resources/views/store/index.blade.php
+++ resources/views/store/index.blade.php
@@ -1,13 +1,25 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<x-template>
 
-        <title>Parcial 1 PCE:: Tienda</title>
+    <x-slot:title>Tienda::</x-slot>
 
-    </head>
-    <body>
-        <h1>Tienda</h1>
-    </body>
-</html>
+    <section class="tienda container-fluid container-md pb-3" id="tienda">
+        <h2 class="titulo-seccion w-75 w-lg-100 text-uppercase text-center my-2 mx-auto px-2">Tienda</h2>
+        <p class="fs-5 w-75 mx-auto">En nuestra tienda encontrarás todo lo <em>necesario para tu entrenamiento</em>. Podrás encontrar paquetes de clases, toda la ropa necesaria (<span lang="ja">keikogi, Obi, Hakamas</span>) de diversos tamaños y calidad, y también podrás encontrar los equipos necesarios (<span lang="ja">Katana, Bokken</span>)</p>
+        <div class="productos pb-3 formularioLogica">
+            <article>
+                <div class="categoria pt-2 pe-2">
+                    <h3 class="titulo-seccion w-75 w-lg-100 text-uppercase text-center my-2 mx-auto px-2"></h3>
+                </div>
+                <div class="categoria pt-2 pe-2 bg-light">
+
+                </div>
+            </article>
+
+            <article id="productos">
+                <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4 my-2 container mx-auto">
+
+                </div>
+            </article>
+        </div>
+    </section>
+</x-template>
