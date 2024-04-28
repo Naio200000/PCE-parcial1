c4ee369859b5ba7e941e2bf00fe27cfb0aa154dc
Naio200000 <90395089+Naio200000@users.noreply.github.com>
2024-04-28 15:40:05 -0300

    feat: add data from db

--- resources/views/store/index.blade.php
+++ resources/views/store/index.blade.php
@@ -2,7 +2,7 @@
     /**
      * @var /App/Models/Producto[] | Illuminate\Database\Eloquent\Collection  $productos
     */
-    
+
 ?>
 
 <x-template>
@@ -20,15 +20,17 @@
             </article>
             <article id="productos">
                 <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4 my-2 container mx-auto">
+
+                    @foreach ($productos as $p)
                     <div class="col">
                         <div class="card tarjetas-producto">
                             <img src="./img/productos/" alt="Imagen del producto" class="card-img-top">
                             <span class="mx-2 ms-auto capital">Categoria</span>
                             <div class="card-body">
-                                <h4 class="card-title fs-2 titulocard" type="button" id=""><a href="#" class="fs-2 titulocard">Un producto</a></h4>
-                                <p class="card-text">Descripcion del producto</p>
+                                <h4 class="card-title fs-2 titulocard" type="button" id=""><a href="#" class="fs-2 titulocard">{{$p->name}}</a></h4>
+                                <p class="card-text">{{$p->descript}}</p>
                                 <div class="row px2">
-                                    <span class="col preciocard">1111.11</span>
+                                    <span class="col preciocard">{{$p->price}}</span>
                                     <div class="col-7">
                                         <div class="row px-0">
                                             <button class="btn btn-komei col text-uppercase me-2"><a class="text-uppercase" href="#">Ver mas</a></button>
@@ -38,42 +40,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="col">
-                        <div class="card tarjetas-producto">
-                            <img src="./img/productos/" alt="Imagen del producto" class="card-img-top">
-                            <span class="mx-2 ms-auto capital">Categoria</span>
-                            <div class="card-body">
-                                <h4 class="card-title fs-2 titulocard" type="button" id=""><a href="#" class="fs-2 titulocard">Un producto</a></h4>
-                                <p class="card-text">Descripcion del producto</p>
-                                <div class="row px2">
-                                    <span class="col preciocard">1111.11</span>
-                                    <div class="col-7">
-                                        <div class="row px-0">
-                                            <button class="btn btn-komei col text-uppercase me-2"><a class="text-uppercase" href="#">Ver mas</a></button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="card tarjetas-producto">
-                            <img src="./img/productos/" alt="Imagen del producto" class="card-img-top">
-                            <span class="mx-2 ms-auto capital">Categoria</span>
-                            <div class="card-body">
-                                <h4 class="card-title fs-2 titulocard" type="button" id=""><a href="#" class="fs-2 titulocard">Un producto</a></h4>
-                                <p class="card-text">Descripcion del producto</p>
-                                <div class="row px2">
-                                    <span class="col preciocard">1111.11</span>
-                                    <div class="col-7">
-                                        <div class="row px-0">
-                                            <button class="btn btn-komei col text-uppercase me-2"><a class="text-uppercase" href="#">Ver mas</a></button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    @endforeach
                 </div>
             </article>
         </div>
