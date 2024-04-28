37c7b143ab29ce0c0595738babae25a81ec40ce8
Naio200000 <90395089+Naio200000@users.noreply.github.com>
2024-04-28 15:24:42 -0300

    feat: add view of producto models

--- app/Http/Controllers/TiendaController.php
+++ app/Http/Controllers/TiendaController.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Producto;
 use Illuminate\Http\Request;
 
 class TiendaController extends Controller{
 
     public function index () {
 
-        return view('store.index');
+        $productos = Producto::all();
+
+
+        return view('store.index', [
+            'productos' => $productos
+        ]);
     }
 }
