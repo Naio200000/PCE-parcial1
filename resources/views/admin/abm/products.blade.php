1259b8f2909c6c496ae41a992122818fa5ff6bf5
Naio200000 <90395089+Naio200000@users.noreply.github.com>
2024-04-28 23:39:20 -0300

    feat: add alert message

--- resources/views/admin/abm/products.blade.php
+++ resources/views/admin/abm/products.blade.php
@@ -1,3 +1,11 @@
+<?php
+
+/**
+ * @var Illuminate\Support\ViewErrorBad $errors
+*/
+?>
+
+
 <x-adminTemplate>
 
     <x-slot:title>ABM - Productos ::</x-slot:title>
@@ -13,6 +21,9 @@
                         @csrf
                         <div class="row align-items-start">
                             <!-- Titulo -->
+                            @if($errors->any())
+                                <div class="alert alert-danger text-center">Hubo un error en la carga del formulario</div>
+                            @endif
                             <div class="mb-3">
                                 <h3 class='text-center fw-bold text-capitalize '>Producto</h3>
                                 <p class="text-center">Los campos marcados con <span class="obligatorio fs-4"> *</span> son obligatorios</p>
@@ -22,16 +33,25 @@
                                 <div class="mb-3 col-12 form-floating">
                                     <input type="text" class="form-control" id="name" placeholder="a" name="name" >
                                     <label for="name" class="col-form-label ms-2">Nombre de la producto<span class="obligatorio fs-4"> *</span></label>
+                                    @error('name')
+                                        <p class="text-danger">{{$message}}</p>
+                                    @enderror
                                 </div>
                                 <!-- Descripcion -->
                                 <div class="mb-3 form-floating">
                                     <textarea class="form-control" id="descript-text" placeholder="a" name="descript" rows="4" style="height:100%;" ></textarea>
                                     <label for="descript-text" class="col-form-label ms-2">Descripción del Producto<span class="obligatorio fs-4"> *</span></label>
+                                    @error('descript')
+                                        <p class="text-danger">{{$message}}</p>
+                                    @enderror
                                 </div>
                                 <!-- precio -->
                                 <div class="mb-3 col-12 form-floating">
                                     <input type="number" class="form-control" id="price" placeholder="1" name="price" >
                                     <label for="price" class="col-form-label ms-2">Precio del Produto<span class="obligatorio fs-4"> *</span></label>
+                                    @error('price')
+                                        <p class="text-danger">{{$message}}</p>
+                                    @enderror
                                 </div>
                             </div>
                             <div class=" col-12 col-sm-6">
@@ -39,17 +59,26 @@
                                 <div class="mb-3 col-12 form-floating">
                                     <input type="text" class="form-control" id="category" placeholder="a" name="category" >
                                     <label for="category" class="col-form-label ms-2">Nombre de la producto<span class="obligatorio fs-4"> *</span></label>
+                                    @error('category')
+                                        <p class="text-danger">{{$message}}</p>
+                                    @enderror
                                 </div>
                                 {{-- imagenes --}}
                                 <div class="mb-3 col-12 form-floating">
                                     <input type="text" class="form-control" id="image" placeholder="a" name="image" >
                                     <label for="image" class="col-form-label ms-2">Cargue una Imagen<span class="obligatorio fs-4"> *</span></label>
                                     {{-- <input class="form-control" type="file" id="imagen" name="imagen"> --}}
+                                    @error('image')
+                                        <p class="text-danger">{{$message}}</p>
+                                    @enderror
                                 </div>
                                 {{-- descripcion de imagen --}}
                                 <div class="mb-3 col-12 form-floating">
                                     <input type="text" class="form-control" id="altImage" placeholder="a" name="altImage" >
                                     <label for="altImage" class="col-form-label ms-2">Descripcion de la imagen<span class="obligatorio fs-4"> *</span></label>
+                                    @error('altImage')
+                                        <p class="text-danger">{{$message}}</p>
+                                    @enderror
                                 </div>
                             </div>
                             <div class="bg-light col-12 mt-3 p-2 d-flex">
