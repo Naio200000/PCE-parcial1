1afc2b89fe7104e5fe7fa157d8c6eb98cacc161e
Naio200000 <90395089+Naio200000@users.noreply.github.com>
2024-04-28 21:07:18 -0300

    feat: add dashboard router

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,5 @@ Route::get('tienda', [\App\Http\Controllers\TiendaController::class, 'index'])->
 
 Route::get('tienda/producto/{id}', [\App\Http\Controllers\TiendaController::class, 'viewProduct'])->name('tienda.producto');
 
+Route::get('admin', [\App\Http\Controllers\AdminController::class,'home'])->name('dash');
+
