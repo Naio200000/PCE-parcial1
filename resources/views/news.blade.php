79766866ca5db4df7d1ebedef8075844161a923d
Naio200000 <90395089+Naio200000@users.noreply.github.com>
2024-05-04 19:06:11 -0300

    minor change

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -10,7 +10,7 @@
 
     </section>
     <section class="home container-fluid container-md">
-        <h2 class="titulo-seccion w-75 w-lg-100 text-uppercase text-center mt-2 mb-5 mx-auto px-2">{{$news->title}}</h2>
+        <h2 class="titulo-seccion w-75 w-lg-100 text-uppercase text-center fw-bold mt-2 mb-5 mx-auto px-2">{{$news->title}}</h2>
         <div>
             <img class="d-block w-100"  src="{{'../img/hero/' . $news->image}}" alt="{{$news->altImage}}">
         </div>
