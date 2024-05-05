3f76532479df1ff3b5d17400fa18da9398221973
Naio200000 <90395089+Naio200000@users.noreply.github.com>
2024-05-04 22:28:25 -0300

    feat: add helpers

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -5,6 +5,28 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * 
+ *
+ * @property int $id
+ * @property string $title
+ * @property string $image
+ * @property string $altImage
+ * @property string $blog
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|Blog newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Blog newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Blog query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Blog whereAltImage($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Blog whereBlog($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Blog whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Blog whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Blog whereImage($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Blog whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Blog whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Blog extends Model
 {
     use HasFactory;
--- app/Models/Producto.php
+++ app/Models/Producto.php
@@ -5,6 +5,32 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * 
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $descript
+ * @property string $image
+ * @property string $altImage
+ * @property string $category
+ * @property int $price
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|Producto newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Producto newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Producto query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Producto whereAltImage($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Producto whereCategory($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Producto whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Producto whereDescript($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Producto whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Producto whereImage($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Producto whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Producto wherePrice($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Producto whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Producto extends Model
 {
     use HasFactory;
