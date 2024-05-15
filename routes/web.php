be011c0abc420a8ccdb0a82a2bfe4d1899ad8c97
Naio200000 <nicolas.alsinet@gmail.com>
2024-05-14 23:13:51 -0300

    feat: add midwaree authentication

--- routes/web.php
+++ routes/web.php
@@ -20,24 +20,24 @@ Route::get('tienda', [\App\Http\Controllers\TiendaController::class, 'index'])->
 Route::get('tienda/producto/{id}', [\App\Http\Controllers\TiendaController::class, 'viewProduct'])->name('tienda.producto')->whereNumber('id');
 
 // Admin
-Route::get('admin', [\App\Http\Controllers\AdminController::class,'home'])->name('dash');
-Route::get('admin/productos', [\App\Http\Controllers\AdminController::class,'products'])->name('products');
-Route::get('admin/blog', [\App\Http\Controllers\AdminController::class,'blog'])->name('blog');
+Route::get('admin', [\App\Http\Controllers\AdminController::class,'home'])->name('dash')->middleware('auth');
+Route::get('admin/productos', [\App\Http\Controllers\AdminController::class,'products'])->name('products')->middleware('auth');
+Route::get('admin/blog', [\App\Http\Controllers\AdminController::class,'blog'])->name('blog')->middleware('auth');
 
 // Abm Productos
-Route::get('admin/agregar/productos', [\App\Http\Controllers\AdminController::class,'addProducts'])->name('add.products');
-Route::get('admin/borrar/productos/{id}', [\App\Http\Controllers\AdminController::class,'delProducts'])->name('del.products')->whereNumber('id');
-Route::get('admin/editar/productos/{id}', [\App\Http\Controllers\AdminController::class,'editProducts'])->name('edit.products')->whereNumber('id');
-Route::post('admin/agregar/productos', [\App\Http\Controllers\AdminController::class,'actionAddProducts'])->name('actions.add.products');
-Route::post('admin/productos/{id}/borrar', [\App\Http\Controllers\AdminController::class,'actionDelProducts'])->name('action.del.products')->whereNumber('id');
-Route::post('admin/productos/{id}/editar', [\App\Http\Controllers\AdminController::class,'actionEditProducts'])->name('actions.edit.products')->whereNumber('id');
+Route::get('admin/agregar/productos', [\App\Http\Controllers\AdminController::class,'addProducts'])->name('add.products')->middleware('auth');
+Route::get('admin/borrar/productos/{id}', [\App\Http\Controllers\AdminController::class,'delProducts'])->name('del.products')->whereNumber('id')->middleware('auth');
+Route::get('admin/editar/productos/{id}', [\App\Http\Controllers\AdminController::class,'editProducts'])->name('edit.products')->whereNumber('id')->middleware('auth');
+Route::post('admin/agregar/productos', [\App\Http\Controllers\AdminController::class,'actionAddProducts'])->name('actions.add.products')->middleware('auth');
+Route::post('admin/productos/{id}/borrar', [\App\Http\Controllers\AdminController::class,'actionDelProducts'])->name('action.del.products')->whereNumber('id')->middleware('auth');
+Route::post('admin/productos/{id}/editar', [\App\Http\Controllers\AdminController::class,'actionEditProducts'])->name('actions.edit.products')->whereNumber('id')->middleware('auth');
 
 // Abm Blogs
-Route::get('admin/abm/blog', [\App\Http\Controllers\AdminController::class,'abmBlog'])->name('abm.blog');
-Route::get('admin/agregar/blog', [\App\Http\Controllers\AdminController::class,'addBlog'])->name('add.blog');
-Route::get('admin/borrar/blog/{id}', [\App\Http\Controllers\AdminController::class,'delBlog'])->name('del.blog')->whereNumber('id');
-Route::get('admin/editar/blog/{id}', [\App\Http\Controllers\AdminController::class,'editBlog'])->name('edit.blog')->whereNumber('id');
-Route::post('admin/agregar/blog', [\App\Http\Controllers\AdminController::class,'actionAddBlog'])->name('actions.add.blog');
-Route::post('admin/blog/{id}/borrar', [\App\Http\Controllers\AdminController::class,'actionDelBlog'])->name('action.del.blog')->whereNumber('id');
-Route::post('admin/blog/{id}/editar', [\App\Http\Controllers\AdminController::class,'actionEditBlog'])->name('actions.edit.blog')->whereNumber('id');
+Route::get('admin/abm/blog', [\App\Http\Controllers\AdminController::class,'abmBlog'])->name('abm.blog')->middleware('auth');
+Route::get('admin/agregar/blog', [\App\Http\Controllers\AdminController::class,'addBlog'])->name('add.blog')->middleware('auth');
+Route::get('admin/borrar/blog/{id}', [\App\Http\Controllers\AdminController::class,'delBlog'])->name('del.blog')->whereNumber('id')->middleware('auth');
+Route::get('admin/editar/blog/{id}', [\App\Http\Controllers\AdminController::class,'editBlog'])->name('edit.blog')->whereNumber('id')->middleware('auth');
+Route::post('admin/agregar/blog', [\App\Http\Controllers\AdminController::class,'actionAddBlog'])->name('actions.add.blog')->middleware('auth');
+Route::post('admin/blog/{id}/borrar', [\App\Http\Controllers\AdminController::class,'actionDelBlog'])->name('action.del.blog')->whereNumber('id')->middleware('auth');
+Route::post('admin/blog/{id}/editar', [\App\Http\Controllers\AdminController::class,'actionEditBlog'])->name('actions.edit.blog')->whereNumber('id')->middleware('auth');
 
