5443ffbc26ed397c52392d2b6af2d6a00fbdddad
Naio200000 <nicolas.alsinet@gmail.com>
2024-05-15 21:45:32 -0300

    feat :add both descript to delete view

--- resources/views/admin/del/product.blade.php
+++ resources/views/admin/del/product.blade.php
@@ -25,7 +25,8 @@ resources/views/store/product.blade.php<?php
                 <div class="col-lg-6 d-flex flex-column p-3">
                     <div class="card-body flex-grow-0">
                         <h3 class="card-title fs-2">{{$product->name}}</h3>
-                        <p class="card-text fs-5">{{$product->descript}}</p>
+                        <p class="card-text fs-5">{{$product->descript[0]}}</p>
+                        <p class="card-text fs-5">{{$product->descript[1]}}</p>
                     </div>
                     <div class="card-body flex-grow-0 mt-auto" id="botonProducto">
                         <p class="fs-3 mb-3 fw-bold text-center">Precio: <span class="preciocard">{{$product->price}}</span></p>
