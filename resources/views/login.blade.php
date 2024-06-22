49780c3ac6b2b0d6cfbb18fa26911c2c08d0bda1
Naio200000 <nicolas.alsinet@gmail.com>
2024-06-21 21:09:14 -0300

    feat: add register button

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -11,7 +11,7 @@
             @endif
             <h2 class="titulo-seccion w-75 w-lg-100 text-uppercase text-center mt-2 mb-5 mx-auto px-2">Inciar Sessión</h2>
             <div class="row my-5 justify-content-center">
-                <div class="col col-md-4">
+                <div class="col col-md-4 p-4 shadow p-3 mb-5 bg-body-tertiary rounded">
                     <form action="{{route('actions.login')}}" method="POST">
                         @csrf
                         <div class="mb-3 form-floating">
@@ -22,7 +22,8 @@
                             <input type="password" class="form-control" id="password" name="password" placeholder="a">
                             <label for="password" class="form-label">Contraseña</label>
                         </div>
-                        <button type="submit" class="btn btn-komei fw-bold ms-auto">Login</button>
+                        <button type="submit" class="btn btn-komei mb-3 fw-bold ms-auto w-100">Login</button>
+                        <button type="submit" class="btn btn-komei bg-primary-subtle fw-bold ms-auto w-100">Registrarse</button>
                     </form>
                 </div>
             </div>
