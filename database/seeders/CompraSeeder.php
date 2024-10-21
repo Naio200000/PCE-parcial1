3ee850c8e6590e27409e1b09c8ba13987a734a4f
Nicolas Alsinet <nicolas.alsinet@gmail.com>
2024-10-21 12:30:46 -0300

    feat: add seeder compras

--- database/seeders/CompraSeeder.php
+++ database/seeders/CompraSeeder.php
@@ -32,7 +32,127 @@ class CompraSeeder extends Seeder
             ],
             [
                 'user_id' => 1,
+                'producto_id' => 6,
+            ],
+            [
+                'user_id' => 3,
+                'producto_id' => 2,
+            ],
+            [
+                'user_id' => 3,
+                'producto_id' => 12,
+            ],
+            [
+                'user_id' => 3,
+                'producto_id' => 8,
+            ],
+            [
+                'user_id' => 3,
+                'producto_id' => 11,
+            ],
+            [
+                'user_id' => 3,
+                'producto_id' => 9,
+            ],
+            [
+                'user_id' => 4,
+                'producto_id' => 1,
+            ],
+            [
+                'user_id' => 4,
+                'producto_id' => 13,
+            ],
+            [
+                'user_id' => 4,
+                'producto_id' => 4,
+            ],
+            [
+                'user_id' => 4,
+                'producto_id' => 16,
+            ],
+            [
+                'user_id' => 4,
+                'producto_id' => 6,
+            ],
+            [
+                'user_id' => 4,
+                'producto_id' => 2,
+            ],
+            [
+                'user_id' => 4,
+                'producto_id' => 12,
+            ],
+            [
+                'user_id' => 4,
+                'producto_id' => 8,
+            ],
+            [
+                'user_id' => 4,
+                'producto_id' => 11,
+            ],
+            [
+                'user_id' => 4,
+                'producto_id' => 9,
+            ],
+            [
+                'user_id' => 6,
+                'producto_id' => 10,
+            ],
+            [
+                'user_id' => 6,
+                'producto_id' => 13,
+            ],
+            [
+                'user_id' => 6,
+                'producto_id' => 4,
+            ],
+            [
+                'user_id' => 6,
+                'producto_id' => 16,
+            ],
+            [
+                'user_id' => 6,
+                'producto_id' => 6,
+            ],
+            [
+                'user_id' => 6,
+                'producto_id' => 2,
+            ],
+            [
+                'user_id' => 6,
+                'producto_id' => 12,
+            ],
+            [
+                'user_id' => 6,
+                'producto_id' => 8,
+            ],
+            [
+                'user_id' => 6,
+                'producto_id' => 11,
+            ],
+            [
+                'user_id' => 6,
+                'producto_id' => 9,
+            ],
+            [
+                'user_id' => 6,
+                'producto_id' => 1,
+            ],
+            [
+                'user_id' => 6,
+                'producto_id' => 14,
+            ],
+            [
+                'user_id' => 6,
                 'producto_id' => 5,
+            ],
+            [
+                'user_id' => 6,
+                'producto_id' => 17,
+            ],
+            [
+                'user_id' => 6,
+                'producto_id' => 7,
             ],
         ]);
     }
